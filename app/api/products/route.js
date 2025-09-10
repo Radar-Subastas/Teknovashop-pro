520b0965f1710590adf3d68f880a03fddffb4d39
Radar-Subastas <radarsubastas@gmail.com>
2025-09-10 14:10:10 +0200

    Delete app directory

--- app/api/products/route.js
+++
@@ -1,8 +0,0 @@
-import products from '@/data/products.json';
-
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-
-export async function GET() {
-  return Response.json({ products });
-}

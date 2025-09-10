bceb637c5124452ba9b578705756601f82c54326
Radar-Subastas <radarsubastas@gmail.com>
2025-09-10 14:10:45 +0200

    Delete components directory

--- components/Footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer(){
-  return (
-    <footer className="border-t border-gray-200">
-      <div className="container py-6 text-sm text-gray-500">
-        © 2025 Teknovashop — Enlaces de afiliado: podemos recibir comisión sin coste extra para ti.
-      </div>
-    </footer>
-  );
-}
